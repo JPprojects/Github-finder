212c214cdab6361f35e142cfb12dec9a73b75496
Jerome <jerome791@hotmail.co.uk>
2019-07-01 20:19:32 +0100

    s

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = true;
-
     return (
       <Fragment>
-        {loading ? <h4>loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <h1>Hello</h1>
       </Fragment>
     );
   }
