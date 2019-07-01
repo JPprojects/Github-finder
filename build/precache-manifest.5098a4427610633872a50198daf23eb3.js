b5bc46fc9100840d30f71308a3fb875412baa154
Jerome <jerome791@hotmail.co.uk>
2019-07-01 19:53:34 +0100

    Set up

---
+++ build/precache-manifest.5098a4427610633872a50198daf23eb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695d2bee579d84c2312aec88991a8828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2866b439c6c7674e10df",
+    "url": "/static/css/main.ae8ec85a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2866b439c6c7674e10df",
+    "url": "/static/js/main.b29ecffe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
